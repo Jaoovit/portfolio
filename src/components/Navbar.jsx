d53e08d164b2c6a4adc09cc064146ca0fdf23221
João Oliveira <jaoovit@gmail.com>
2024-07-03 20:15:15 +0100

    define the links in the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
   return (
     <div>
-      
+      <Link to='/'>Home</Link>
+      <Link to='/projects'>Projects</Link>
+      <Link to='/articles'>Articles</Link>
+      <Link to='/about'>About Me</Link>
+      <Link to='/contact'>Contact Me</Link>
     </div>
   )
 }
