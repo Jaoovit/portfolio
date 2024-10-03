ac6c170ca427a491817cf3b2aa769e1870e6fb07
João Oliveira <jaoovit@gmail.com>
2024-10-03 18:10:55 +0100

    add a blog-api project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,6 @@
 import movieLibraryImage from "../assets/movie-library-image.png";
 import fileUploaderImage from "../assets/file-uploader-image.png";
+import blogApiImage from "../assets/blog-api-image.png";
 
 const Projects = () => {
   const projectData = [
@@ -14,6 +15,12 @@ const Projects = () => {
       description: "A storage service that allows users to store and share files. With its user-friendly interface, you can upload documents, photos, and videos, access them from anywhere.",
       link: "https://file-uploader-q6uq.onrender.com/",
       image: fileUploaderImage
+    },
+    {
+      title: "Blog API",
+      description: "A RESTful API built with Node.js, Express, featuring user authentication with JWT and CRUD operations for managing blog. It uses Sequelize for database interactions",
+      link: "https://github.com/Jaoovit/blog-API",
+      image: blogApiImage
     }
   ];
 
@@ -23,27 +30,28 @@ const Projects = () => {
       <div className="flex flex-col gap-10">
         {projectData.map((project, index) => (
           <div key={index} className="flex flex-col md:flex-row border p-6 rounded-lg shadow-md">
-            <div className="flex justify-center items-center border border-gray-200 p-4 rounded-md">
-              <img 
-                src={project.image} 
-                alt={project.title} 
-                className="h-48 object-cover rounded-md" 
-                style={{ height: '300px' }}
-              />
-            </div>
-            <div className="flex flex-col justify-center items-center md:w-1/2 text-center">
-              <h2 className="text-2xl font-semibold">{project.title}</h2>
-              <p className="mt-2 mb-4">{project.description}</p>
-              <a 
-                href={project.link} 
-                target="_blank" 
-                rel="noopener noreferrer" 
-                className="text-blue-500 hover:underline"
-              >
-                View Project
-              </a>
-            </div>
+          <div className="w-full md:w-1/2 flex justify-center items-center border border-gray-200 p-4 rounded-md">
+            <img 
+              src={project.image} 
+              alt={project.title} 
+              className="h-48 object-cover rounded-md" 
+              style={{ height: '300px' }} // Keeps the image height consistent
+            />
           </div>
+          <div className="flex flex-col justify-center items-center md:w-1/2 text-center">
+            <h2 className="text-2xl font-semibold">{project.title}</h2>
+            <p className="mt-2 mb-4">{project.description}</p>
+            <a 
+              href={project.link} 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className="text-blue-500 hover:underline"
+            >
+              View Project
+            </a>
+          </div>
+        </div>
+        
         ))}
       </div>
     </div>
